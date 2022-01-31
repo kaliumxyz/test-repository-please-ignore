31dc44c7558c59345db70796b9aa408eea915006
kaliumxyz <git@kalium.xyz>
2022-01-31 22:37:44 +0100

    intermittent version for history reasons

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 #!/usr/bin/env node
-"use strict"
 /* native */
-const child = require('child_process')
-const fs = require('fs')
-const path = require('path')
-const os = require('os')
+import child from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
 
 
 /* npm */
-const parseArgs = require('minimist')(process.argv)
-const chalk = require('chalk')
+import args from 'args';
+import chalk from 'chalk';
 
-const settings = {
-	repo: parseArgs.dir || "test",
-	date: parseArgs.time || "2000-01-01 00:00:01" // why 2k?
-}
+args
+  .option('repo', 'the name of the repo', 'history')
+  .option('start', 'Date to begin at', '1990-01-01 00:00:01')
+  .option('end', 'Date to end at', '2099-01-01 00:00:01')
 
-fs.mkdir(settings.repo, console.error)
-fs.writeFile(path.join(settings.repo, "test"),"wat", console.error)
+const flags = args.parse(process.argv)
 
-// doesn't work on windows, so terminates (probably works on linux)
-if(os.type() === 'Windows_NT'){
-	console.log(chalk.bgRed("Windows is not supported </3"))
-	process.exit(1)
-}
+console.log()
 
-// date should always be yyyy-mm-dd hh:mm:ss
-child.exec(`git init; export GIT_COMMITTER_DATE="${settings.date}" && export GIT_AUTHOR_DATE="${settings.date}" && git commit -am "init"`, {cwd: settings.repo, shell: true}).stderr.pipe(process.stderr)
+fs.mkdir(flags.repo, console.error)
+fs.writeFile(path.join(flags.repo, "readme.md"),`
+#history
+
+I am a really good developer look at my git timeline like woah all green all the time forever
+`, console.error)
+
+// init
+child.exec(`
+    git init
+    export GIT_COMMITTER_DATE="${flags.start}"
+    export GIT_AUTHOR_DATE="${flags.start}"
+    git commit -am "init"
+    for i in $(seq 12000 1);
+       do date -v "-$i"d +%Y-%m-%d\ %H:%M:%S;
+    done
+`, {cwd: settings.repo, shell: true}).stderr.pipe(process.stderr)
+// // date should always be yyyy-mm-dd hh:mm:ss
+// child.exec(`
+//     export GIT_COMMITTER_DATE="${flags.start}"
+//     export GIT_AUTHOR_DATE="${flags.start}"
+//     git commit -am "init"
+// `, {cwd: settings.repo, shell: true}).stderr.pipe(process.stderr)
